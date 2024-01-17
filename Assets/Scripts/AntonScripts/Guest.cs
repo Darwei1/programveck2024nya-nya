69c10ed0c92ebb3d7af0fc194e8b423d27486d6d
AntonBaksa <anton.baksa@elev.ga.lbs.se>
2024-01-17 12:50:05 +0100

    j

--- Assets/Scripts/AntonScripts/Guest.cs
+++ Assets/Scripts/AntonScripts/Guest.cs
@@ -8,22 +8,15 @@ public class Guest : MonoBehaviour
 
     public GameObject objectToSpawn;
 
-    
-    public GameObject button;
+
     public Button buttonScript;
 
     // Start is called before the first frame update
     void Start()
     {
 
-        GameObject KeyButton = GameObject.FindWithTag("Button");
-
-        Button button = buttonScript.GetComponentInChildren<Button>();
-
-        
-        buttonScript.spawn = true;
-
        
+
     }
 
     // Update is called once per frame
@@ -32,7 +25,9 @@ public class Guest : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F))//press F, do action
         {
             Instantiate(objectToSpawn, new Vector3(0, 4, 0), Quaternion.identity);//Instatiate object at 0,4,0
-            
+            buttonScript.spawn = true;
         }
+
+        
     }
 }
