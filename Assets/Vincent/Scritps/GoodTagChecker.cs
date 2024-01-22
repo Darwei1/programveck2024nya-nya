dcbd98d6e18f83d00fd5ef7751c170016c65a727
Darwei1 <vincent.fajersson@elev.ga.lbs.se>
2024-01-22 15:20:35 +0100

    push POSITIVITY

--- Assets/Vincent/Scritps/GoodTagChecker.cs
+++ Assets/Vincent/Scritps/GoodTagChecker.cs
@@ -2,15 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TheMagnumOpus : MonoBehaviour 
+public class GoodTagChecker : MonoBehaviour
 {
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("A"))
         {
-            Debug.Log("the magnus opus");
+            Debug.Log("Player is Correct");
         }
-    }
+        else Debug.LogWarning("Player is Incorrect");
 
+
+    }
 }
