a383d31ccf902f9b479274eeab54d4931fd56fc2
HugoIhlman <hugo.ihlman@elev.ga.lbs.se>
2024-01-24 10:45:00 +0100

    f

--- Assets/Hugo/Scripts/KeySpawn.cs
+++ Assets/Hugo/Scripts/KeySpawn.cs
@@ -6,16 +6,31 @@ public class KeySpawn : MonoBehaviour
 {
     public GameObject objectToSpawn;
 
-
-    private bool spawn = true;// its true
+    public GameObject pref;
+    public bool spawn;// its true
     // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+     void Update()
+    {
+        pref = GameObject.FindGameObjectWithTag("guest");
+        
+        
+    }
     public void OnButtonPress()
     {
+        if (pref != null)
+        {
+            spawn = true;
+        }
         if (spawn == true)
         {
             Instantiate(objectToSpawn, new Vector3(3, -3, 0), Quaternion.Euler(0, 0, -90));
-            
+            spawn = false;
         }
 
     }
+    
 }
